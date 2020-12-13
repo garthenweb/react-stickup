a513534f42e797e0612f331deb5efad91d7d2acf
Jannick Garthen <jannick.garthen@gmail.com>
2020-12-13 06:55:27 +0100

    test(ci): fix test setup for node v10

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   preset: 'ts-jest',
   globals: {
     'ts-jest': {
-      tsConfig: './tsconfig.test.json',
+      tsconfig: './tsconfig.test.json',
       diagnostics: {
         warnOnly: true,
       },
